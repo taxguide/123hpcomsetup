655c9be89641a3df936b9e2bb25a1189f869ab65
taxguide <singhparash06@gmail.com>
2026-07-29 12:49:02 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,21 @@
+# Configuration file for the Sphinx documentation builder.
 
+project = '123.hp.com/setup'
+author = '123.hp.com/setup'
+release = '1.0'
+
+extensions = []
+
+# Templates
+templates_path = ['_templates']
+
+exclude_patterns = []
+
+html_theme = 'alabaster'
+
+# Static files
+html_static_path = ['_static']
+
+language = 'en'
+
+html_title = "123.hp.com/setup"
